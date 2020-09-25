384c2ec8288560101c25cbc4e43d2a6c5c6ede48
Rain <ryanalacombe@gmail.com>
2020-09-25 15:25:38 -0500

    Added Ragdolls and Player Movement

---
+++ Assets/Scripts/PlayerMovementBehavior.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(Animator))]
+public class PlayerMovementBehavior : MonoBehaviour
+{
+    private CharacterController _controller = null;
+    private Animator _animator = null;
+
+    public float speed = 1.0f;
+    public float turnRate = 1.0f;
+    public float pushPower = 2.0f;
+
+    public bool tankControls = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 desiredMovement = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+
+        if (tankControls)
+        {
+            //Tank movement
+            _controller.SimpleMove(transform.forward * desiredMovement.z * speed);
+            transform.Rotate(transform.up, desiredMovement.x * turnRate);
+        }
+        else
+        {
+            //World-based movement
+            _controller.SimpleMove(desiredMovement);
+            if (desiredMovement.magnitude > 0)
+                transform.forward = desiredMovement.normalized;
+        }
+        //Animate based on movement
+        _animator.SetFloat("Speed", desiredMovement.z * speed);
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Rigidbody otherRB = hit.rigidbody;
+        //Stop if there is no rigidbody or if that rigidboy is kinematic
+        if (otherRB == null || otherRB.isKinematic)
+            return;
+
+        if (hit.moveDirection.y < -0.3f)
+            return;
+
+        Vector3 pushDirection = new Vector3(hit.moveDirection.x, 0.0f, hit.moveDirection.z);
+        //otherRB.velocity = pushDirection * pushPower;
+        otherRB.AddForceAtPosition(pushDirection * pushPower, hit.point);
+    }
+}
---
+++ Assets/Scripts/RagdollBehavior.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class RagdollBehavior : MonoBehaviour
+{
+    private Animator _animator = null;
+    private CharacterController _controller = null;
+
+    public List<Rigidbody> rigidbodies = new List<Rigidbody>();
+
+    public bool ragdollEnabled
+    {
+        get { return !_animator.enabled; }
+        set
+        {
+            _animator.enabled = !value;
+            _controller.enabled = !value;
+            foreach (Rigidbody rb in rigidbodies)
+            {
+                rb.isKinematic = !value;
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _controller = GetComponent<CharacterController>();
+        ragdollEnabled = false;
+    }
+}
